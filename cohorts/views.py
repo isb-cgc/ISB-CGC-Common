ce286860bac99e4ee971ac4ff80bb7f1a5dda717
s-paquette <spaquett@systemsbiology.org>
2016-06-29 10:11:19 -0700

    -> cohorts/views now has its own database connection method

--- cohorts/views.py
+++ cohorts/views.py
@@ -20,6 +20,7 @@ import json
 import collections
 import csv
 import sys
+import os
 import time
 import logging
 import json
@@ -27,6 +28,7 @@ import traceback
 import copy
 import urllib
 import re
+import MySQLdb
 
 from django.utils import formats
 from django.shortcuts import render, redirect
@@ -64,6 +66,29 @@ MAX_SEL_FILES = settings.MAX_FILES_IGV
 
 logger = logging.getLogger(__name__)
 
+
+# Database connection - does not check for AppEngine
+def get_sql_connection():
+    env = os.getenv('SERVER_SOFTWARE')
+    database = settings.DATABASES['default']
+    try:
+        connect_options = {
+            'host': database['HOST'],
+            'db': database['NAME'],
+            'user': database['USER'],
+            'passwd': database['PASSWORD']
+        }
+
+        if 'OPTIONS' in database and 'ssl' in database['OPTIONS']:
+            connect_options['ssl'] = database['OPTIONS']['ssl']
+
+        db = MySQLdb.connect(**connect_options)
+    except:
+        print >> sys.stderr, "Unexpected ERROR in get_sql_connection(): ", sys.exc_info()[0]
+
+    return db
+
+
 def convert(data):
     if isinstance(data, basestring):
         return str(data)
@@ -86,7 +111,7 @@ METADATA_API = settings.BASE_API_URL + '/_ah/api/meta_api/'
 # TODO: needs to be refactored to use other samples tables
 def get_participant_count(sample_ids):
 
-    db = sql_connection()
+    db = get_sql_connection()
     cursor = None
 
     try:
@@ -137,7 +162,7 @@ def count_metadata(user, cohort_id=None, sample_ids=None, filters=None):
             'sample_barcode': build_where_clause({'sample_barcode': samples_by_study}),
         }
 
-    db = sql_connection()
+    db = get_sql_connection()
     django.setup()
 
     try:
@@ -320,7 +345,7 @@ def query_samples_and_studies(parameter, bucket_by=None):
         bucket_by = None
 
     try:
-        db = sql_connection()
+        db = get_sql_connection()
         cursor = db.cursor(MySQLdb.cursors.DictCursor)
         start = time.time()
         cursor.execute(query_str, (parameter,))
@@ -395,7 +420,7 @@ def metadata_counts_platform_list(req_filters, cohort_id, user, limit):
         + ": " + (stop - start).__str__()
     )
 
-    db = sql_connection()
+    db = get_sql_connection()
 
     query_str = "SELECT " \
                 "IF(has_Illumina_DNASeq=1, " \
