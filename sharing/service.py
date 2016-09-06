5e73585112907812dddabbf594836cc806edc205
phyllers <phyllers@gmail.com>
2016-09-06 11:24:11 -0700

    trying to fix mailgun headers

--- sharing/service.py
+++ sharing/service.py
@@ -86,7 +86,8 @@ def create_share(request, item, emails, type, share_user=None):
             'to': email,
             'subject': 'You Were Added on a ' + type,
             'text': email_text_template.render(ctx),
-            'html': email_template.render(ctx)
+            'html': email_template.render(ctx),
+            'h:x-mailgun-native-send': 'true'
         }
 
         send_email_message(message_data)
