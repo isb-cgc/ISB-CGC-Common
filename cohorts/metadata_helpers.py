f945a9c10a3462d0e341f62b4677f7176d337965
s-paquette <spaquett@systemsbiology.org>
2017-01-24 16:31:00 -0800

    -> Removed changes not needed per bugfixes

--- cohorts/metadata_helpers.py
+++ cohorts/metadata_helpers.py
@@ -133,7 +133,7 @@ def fetch_program_attr(program):
 
             db = get_sql_connection()
             cursor = db.cursor()
-            cursor.callproc('get_program_attr', [str(program)])
+            cursor.callproc('get_program_attr', (program,))
             for row in cursor.fetchall():
                 METADATA_ATTR[program][row[0]] = {'name': row[0], 'displ_name': format_for_display(row[0]) if row[0] not in preformatted_attr else row[0], 'values': {}, 'type': row[1]}
 
@@ -232,7 +232,7 @@ def fetch_metadata_value_set(program=None):
             db = get_sql_connection()
             cursor = db.cursor()
 
-            cursor.callproc('get_metadata_values', [program])
+            cursor.callproc('get_metadata_values', (program,))
 
             for row in cursor.fetchall():
                 METADATA_ATTR[program][cursor.description[0][0]]['values'][str(row[0])] = format_for_display(str(row[0])) if cursor.description[0][0] not in preformatted_values else str(row[0])
@@ -243,7 +243,7 @@ def fetch_metadata_value_set(program=None):
 
             cursor.close()
             cursor = db.cursor(MySQLdb.cursors.DictCursor)
-            cursor.callproc('get_program_display_strings', [program])
+            cursor.callproc('get_program_display_strings', (program,))
 
             for row in cursor.fetchall():
                 if row['value_name'] is not None and row['attr_name'] in METADATA_ATTR[program] and row['value_name'] in METADATA_ATTR[program][row['attr_name']]['values']:
