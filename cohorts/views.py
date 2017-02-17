45d923afb21cf570a17bfe651c4e1958ea716838
s-paquette <spaquett@systemsbiology.org>
2017-02-17 15:08:09 -0800

    -> Loglines

--- cohorts/views.py
+++ cohorts/views.py
@@ -1880,13 +1880,13 @@ def set_operation(request):
 
                 notes = 'Subtracted ' + (', '.join(Cohort.objects.filter(id__in=subtract_ids).values_list('name', flat=True))) + (' from %s.' % base_cohort.name)
 
-                print >> sys.stdout, "[STATUS] Notes recorded at "+str(time.time())
+                print >> sys.stdout, "[STATUS] Notes recorded, length of samples: "+str(len(samples))
 
-                print >> sys.stdout, "[STATUS] Length of samples: "+str(len(samples))
+            print >> sys.stdout, "[STATUS] POST IF/ELSE"
 
             if len(samples):
 
-                print >> sys.stdout, "[STATUS] Making cohort and permissions at "+str(time.time())
+                print >> sys.stdout, "[STATUS] Making cohort and permissions"
 
                 new_cohort = Cohort.objects.create(name=name)
                 perm = Cohort_Perms(cohort=new_cohort, user=request.user, perm=Cohort_Perms.OWNER)
