95e01c860f54529b9cd49229728ebd4987f6f00f
wlongabaugh <wlongabaugh@systemsbiology.org>
2017-03-21 14:48:19 -0700

    Fix in Common for Issue 1795

--- projects/views.py
+++ projects/views.py
@@ -105,10 +105,23 @@ def program_upload(request, existing_proj=False):
     else:
         template = 'projects/program_upload.html'
 
+    have_a_bucket = False
+    for google_project in google_projects:
+        if google_project.bucket_set.all().count() > 0:
+            have_a_bucket = True
+            break
+
+    have_a_dataset = False
+    for google_project in google_projects:
+        if google_project.bqdataset_set.all().count() > 0:
+            have_a_dataset = True
+            break
+
     programs = Program.objects.filter(owner=request.user, active=True) | Program.objects.filter(is_public=True, active=True)
 
-
     context = {
+        'got_bucket' : have_a_bucket,
+        'got_dataset': have_a_dataset,
         'requested': False,
         'programs': programs,
         'google_projects': google_projects,
