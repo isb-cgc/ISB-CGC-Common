0a6fe638175c5aa85eeb7f2c01e34c3e60b02820
kleinone <kleinone@systemsbiology.org>
2017-06-23 20:13:08 -0700

    Updated tests

--- accounts/tests/test_acl_action_runner.py
+++ accounts/tests/test_acl_action_runner.py
@@ -25,7 +25,8 @@ from django.utils import timezone
 
 from django.contrib.auth.models import User
 from accounts.models import AuthorizedDataset, NIH_User, GoogleProject, ServiceAccount, UserAuthorizedDatasets
-from tasks.nih_whitelist_processor.auth_list_processor.nih_auth_list import NIHDatasetAuthorizationList
+#from tasks.nih_whitelist_processor.auth_list_processor.nih_auth_list import NIHDatasetAuthorizationList
+from dataset_utils.nih_auth_list import NIHDatasetAuthorizationList
 from tasks.nih_whitelist_processor.utils import DatasetToACLMapping
 from tasks.nih_whitelist_processor.acl_group_util import ACLGroupSupportSimulator
 from tasks.nih_whitelist_processor.django_utils import AccessControlUpdater, \
--- accounts/tests/test_whitelist_multi_acl.py
+++ accounts/tests/test_whitelist_multi_acl.py
@@ -24,7 +24,7 @@ from django.test import TestCase
 
 from django.contrib.auth.models import User
 from accounts.models import AuthorizedDataset, NIH_User, GoogleProject, ServiceAccount, UserAuthorizedDatasets
-from tasks.nih_whitelist_processor.auth_list_processor.nih_auth_list import NIHDatasetAuthorizationList
+from dataset_utils.nih_auth_list import NIHDatasetAuthorizationList
 from tasks.nih_whitelist_processor.utils import DatasetToACLMapping
 from tasks.nih_whitelist_processor.django_utils import AccessControlUpdater
 from tasks.tests.data_generators import create_csv_file_object
