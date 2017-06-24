c77fee99de46d0e4de34b3e539aa897f7a019de6
kleinone <kleinone@systemsbiology.org>
2017-06-23 20:17:32 -0700

    Added tests module

---
+++ dataset_utils/tests/test_dataset_config.py
@@ -0,0 +1,35 @@
+"""
+
+Copyright 2017, Institute for Systems Biology
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+"""
+
+from unittest import TestCase
+
+from .data_generators import create_csv_file_object
+
+
+class test_one_file(TestCase):
+    def test_one_line(self):
+        test_csv_data = [
+            ['User McName', 'USERNAME1', 'eRA', 'PI', 'username@fake.com', '555-555-5555', 'active', 'phs123456.v1.p1.c1',
+             'General Research Use', '2013-01-01 12:34:56.789', '2014-06-01 16:00:00.100', '2017-06-11 00:00:00.000', '']
+        ]
+
+        data = create_csv_file_object(test_csv_data, include_header=True)
+
+        parser = NIHDatasetAuthorizationList.from_stream(data)
+
+
