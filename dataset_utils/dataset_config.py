9f41403c1a23b388f630db0db0795d5b8d8b0894
s-paquette <spaquett@systemsbiology.org>
2017-07-07 13:08:02 -0700

    -> Fix for eRA dataset parsing

--- dataset_utils/dataset_config.py
+++ dataset_utils/dataset_config.py
@@ -156,13 +156,15 @@ class DatasetAccessSupport(object):
         self.authorization_list_map[dataset_id] = auth_list_instance
         return auth_list_instance
 
-    def is_era_login_in_authorization_list(self, era_login_name, dataset_id):
+    def is_era_login_in_authorization_list(self, era_email, dataset_id):
         auth_list = self.get_auth_list_instance_for_dataset_id(dataset_id)
-        return auth_list.is_era_login_active(era_login_name)
+        return auth_list.is_era_login_active(era_email)
 
     def get_datasets_for_era_login(self, user=None):
         """
         Answer the data sets an ERA user has access to.
+
+        @user: eRA-linked email address which will be present in the dbGaP whitelist
 
         Returns: Array of DatasetGoogleGroupPair instances.
         """
