d63f9b428d9e2d26648ba4d43cb84d81df001758
s-paquette <spaquett@systemsbiology.org>
2018-02-21 11:38:56 -0800

    -> Hopefully final fixes for #2215

--- accounts/views.py
+++ accounts/views.py
@@ -337,10 +337,14 @@ def verify_gcp(request, user_id):
         gcp_id = request.GET.get('gcp-id', None)
         is_refresh = bool(request.GET.get('is_refresh', '')=='true')
 
-        gcp = GoogleProject.objects.filter(project_id=gcp_id, active=1)
-        # Can't register the same GCP twice - return immediately
-        if gcp.count() > 0 and not is_refresh:
-            return JsonResponse({'message': 'A Google Cloud Project with the project ID {} has already been registered.'.format(str(gcp.project_id))}, status='500')
+        try:
+            gcp = GoogleProject.objects.get(project_id=gcp_id, active=1)
+            # Can't register the same GCP twice - return immediately
+            if not is_refresh:
+                return JsonResponse({'message': 'A Google Cloud Project with the project ID {} has already been registered.'.format(str(gcp.project_id))}, status='500')
+        except ObjectDoesNotExist:
+            if is_refresh:
+                return JsonResponse({'message': 'GCP ID {} does not exist and so cannot be refreshed'.format(str(gcp_id))}, status='500')
 
         crm_service = get_special_crm_resource()
         iam_policy = crm_service.projects().getIamPolicy(
@@ -403,6 +407,8 @@ def register_gcp(request, user_id):
             register_users = request.POST.getlist('register_users')
             is_refresh = bool(request.POST.get('is_refresh', '') == 'true')
 
+            logger.debug("[STATUS] register_users: {}".format(str(register_users)))
+
             project = crm_service.projects().get(projectId=project_id).execute()
 
             project_name = project['name']
@@ -414,6 +420,7 @@ def register_gcp(request, user_id):
             elif not project_id:
                 raise Exception("Project ID not provided.")
             elif not len(register_users) or not gcp_users.count():
+                logger.debug("[STATUS] register_users count is {}, gcp_users.count is {}".format(str(len(register_users)),str(gcp_users.count())))
                 # A set of users to register or refresh is required
                 msg = "[STATUS] No registered user set found for GCP {} of project {}; {} aborted.".format(
                     "refresh" if is_refresh else "registration",project_id,"refresh" if is_refresh else "registration")
