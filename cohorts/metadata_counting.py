57afde0127ec029f4f6de2435d013adb97c28ceb
S. Paquette <spaquett@systemsbiology.org>
2018-05-10 17:22:31 -0700

    -> Fixed an error in a messages response -> Prep to make genomic builds NULL-allowed in data type avail. tables

--- cohorts/metadata_counting.py
+++ cohorts/metadata_counting.py
@@ -377,8 +377,6 @@ def count_public_metadata(user, cohort_id=None, inc_filters=None, program_id=Non
                                           hugo_symbol=str(params['gene']), var_class=params['var_class'],
                                           cohort_dataset=bq_cohort_dataset, cohort_table=bq_cohort_table, cohort=cohort)
 
-            print >> sys.stdout, str(query)
-
             bq_service = authorize_credentials_with_Google()
             query_job = submit_bigquery_job(bq_service, settings.BQ_PROJECT_ID, query)
             job_is_done = is_bigquery_job_finished(bq_service, settings.BQ_PROJECT_ID, query_job['jobReference']['jobId'])
@@ -586,7 +584,7 @@ def count_public_metadata(user, cohort_id=None, inc_filters=None, program_id=Non
                 """ % (filter_table, data_avail_table, data_type_table, cohort_join)
 
                 data_avail_query = """
-                    SELECT DISTINCT ms.sample_barcode, GROUP_CONCAT(CONCAT(dt.isb_label,'; ',dt.genomic_build))
+                    SELECT DISTINCT ms.sample_barcode, GROUP_CONCAT(CONCAT(dt.isb_label,'; ',COALESCE(dt.genomic_build,'Avail.')))
                     FROM %s ms
                     JOIN %s da ON da.sample_barcode = ms.sample_barcode
                     JOIN %s dt ON dt.metadata_data_type_availability_id = da.metadata_data_type_availability_id
@@ -608,7 +606,7 @@ def count_public_metadata(user, cohort_id=None, inc_filters=None, program_id=Non
                 """ % (base_table, data_avail_table, data_type_table,)
 
                 data_avail_query = """
-                    SELECT DISTINCT ms.sample_barcode, GROUP_CONCAT(CONCAT(dt.isb_label,'; ',dt.genomic_build))
+                    SELECT DISTINCT ms.sample_barcode, GROUP_CONCAT(CONCAT(dt.isb_label,'; ',COALESCE(dt.genomic_build,'Avail.')))
                     FROM %s ms
                     JOIN %s da ON da.sample_barcode = ms.sample_barcode
                     JOIN %s dt ON dt.metadata_data_type_availability_id = da.metadata_data_type_availability_id
