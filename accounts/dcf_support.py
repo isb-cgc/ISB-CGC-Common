2fc1c96da61b3dad1df4b45e6a7fe90d4a72780f
wlongabaugh <wlongabaugh@systemsbiology.org>
2018-11-06 16:12:24 -0800

    Handle missing key in DCF response, debugging list index problem

--- accounts/dcf_support.py
+++ accounts/dcf_support.py
@@ -511,17 +511,30 @@ def _write_project_member_summary(member_info, project_id):
     """
     We get back a dictionary of results from DCF for project membership. Take this dict and write a summary string to
     show the user. Returns a tuple of (success, message)
+    11/6/18: Seeing at least first key missing if monitoring SA not present. Harden this to handle.
     """
 
-    in_fence = member_info["members_exist_in_fence"]
+    have_fence_key = "members_exist_in_fence" in member_info
+    in_fence = member_info["members_exist_in_fence"] if have_fence_key else None
     not_in_fence = in_fence is not None and not in_fence
     is_in_fence = in_fence is not None and in_fence
-    fence_message = "Not all project members have registered with the Data Commons Framework" if not_in_fence else None
+    if not have_fence_key:
+        fence_message = "Could not obtain project members"
+    elif not_in_fence:
+        fence_message = "Not all project members have registered with the Data Commons Framework"
+    else:
+        fence_message = None
 
-    valid_members = member_info["valid_member_types"]
+    have_valid_key = "valid_member_types" in member_info
+    valid_members = member_info["valid_member_types"] if have_valid_key else None
     not_valid_members = valid_members is not None and not valid_members
     is_valid_members = valid_members is not None and valid_members
-    member_message = "Project {} has one or more Google groups as members".format(project_id) if not_valid_members else None
+    if not have_valid_key:
+        member_message = "Could not obtain project members"
+    elif not_valid_members:
+        member_message = "Project {} has one or more Google groups as members".format(project_id)
+    else:
+        member_message = None
 
     is_ok = is_in_fence and is_valid_members
     combination = []
@@ -541,20 +554,32 @@ def _write_dataset_summary(dataset_info, dataset_id, phs_map):
     """
     We get back a dictionary of results from DCF for project membership. Take this dict and write a summary string to
     show the user. Returns a tuple of (success, message)
+    11/6/18: Since above routing hardened to handle missing keys, do this here too.
     """
 
     full_name = '{} ({})'.format(phs_map[dataset_id], dataset_id)
 
-
-    all_access = dataset_info["all_users_have_access"]
+    have_access_key = "all_users_have_access" in dataset_info
+    all_access = dataset_info["all_users_have_access"] if have_access_key else None
     not_all_access = all_access is not None and not all_access
     is_all_access = all_access is not None and all_access
-    access_message = 'Not all project members have access to dataset "{}"'.format(full_name) if not_all_access else None
+    if not have_access_key:
+        access_message = 'Could not obtain project members'
+    elif not_all_access:
+        access_message = 'Not all project members have access to dataset "{}"'.format(full_name)
+    else:
+        access_message = None
 
-    set_exists = dataset_info["exists"]
+    have_exists_key = "exists" in dataset_info
+    set_exists = dataset_info["exists"] if have_exists_key else None
     not_set_exists = set_exists is not None and not set_exists
     is_set_exists = set_exists is not None and set_exists
-    exists_message = 'Dataset "{}" does not exist'.format(full_name) if not_set_exists else None
+    if not have_exists_key:
+        exists_message = 'Could not obtain project members'
+    elif not_set_exists:
+        exists_message = 'Dataset "{}" does not exist'.format(full_name)
+    else:
+        exists_message = None
 
     is_ok = is_all_access and is_set_exists
     combination = []
