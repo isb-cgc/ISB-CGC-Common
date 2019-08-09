a96df29254c4bd0069a86e319a9a49b067475667
S. Paquette <spaquett@systemsbiology.org>
2019-08-09 12:20:06 -0700

    -> EoF

--- cohorts/tests.py
+++ cohorts/tests.py
@@ -14,13 +14,6 @@
 # limitations under the License.
 #
 
-
-# This file demonstrates writing tests using the unittest module. These will pass
-# when you run "manage.py test".
-#
-# Replace this with more appropriate tests for your application.
-
-from django import http
 from django.test import TestCase
 from django.contrib.auth.models import AnonymousUser, User
 
