e6ecce5dd371baf8bfddb9f0d9e30c19d204531e
S. Paquette <spaquett@systemsbiology.org>
2019-08-21 16:08:15 -0700

    -> Wrong package...

--- cohorts/utils.py
+++ cohorts/utils.py
@@ -18,7 +18,8 @@ from __future__ import absolute_import
 from builtins import map
 from builtins import next
 from builtins import str
-from past.builtins import basestring, old_div
+from past.builtins import basestring
+from past.utils import old_div
 from builtins import object
 
 from .models import Cohort, Samples, Cohort_Perms, Source, Filters, Cohort_Comments
