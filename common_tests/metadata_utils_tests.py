07d91b1e8c6216c757c4eeb1206c10903881c478
S. Paquette <spaquett@systemsbiology.org>
2020-01-08 11:55:04 -0800

    -> Basic unit test setup

---
+++ common_tests/metadata_utils_tests.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class TestMetadataUtils(unittest.TestCase):
+
+    def test_something(self):
+        print("Testing something")
+        self.assertEqual('foo'.upper(), 'FOO')
+
+
+if __name__ == '__main__':
+    unittest.main()
