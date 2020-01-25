c1722d3aeb49c53149ad46d4902dd60e91ff3701
William Clifford <bcliffor@systemsbiology.org>
2020-01-25 10:37:15 -0800

    Added API routes/views to get list of programs and collections

--- idc_collections/urls.py
+++ idc_collections/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.program_list, name='programs'),
     url(r'^public/$', views.public_program_list, name='public_programs'),
+    url(r'^api/public/$', views.public_program_list_api, name='public_programs_api'),
     url(r'^upload/$', views.program_upload, name="program_upload"),
     url(r'^upload/existing$', views.program_upload_existing, name="program_upload_existing"),
     url(r'^(?P<program_id>\d+)/$', views.program_detail, name='program_detail'),
@@ -13,6 +14,7 @@ urlpatterns = [
     url(r'^(?P<program_id>\d+)/edit/?$', views.program_edit, name="program_edit"),
     url(r'^(?P<program_id>\d+)/share/?$', views.program_share, name="program_share"),
     url(r'^(?P<program_id>\d+)/unshare/?$', views.program_unshare, name="program_unshare"),
+    url(r'^api/(?P<program_name>[A-Za-z0-9_-]+)/$', views.program_detail_api, name='program_detail_api'),
     url(r'^data/$', views.upload_files, name='program_file_upload'),
     url(r'^system_data_dict/$', views.system_data_dict, name='system_data_dict'),
     # url(r'^request/$', views.request_program, name="program_request_result"),
