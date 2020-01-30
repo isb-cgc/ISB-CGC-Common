ac0cff8b5d7648cefe55793fbe1dab63e42df20f
s-paquette <spaquett@systemsbiology.org>
2020-01-30 12:28:33 -0800

    -> Use or for now...

--- cohorts/decorators.py
+++ cohorts/decorators.py
@@ -5,7 +5,7 @@ from django.conf import settings
 
 def api_only(view_func):
     def authorize(request, *args, **kwargs):
-        if request.get_host() == settings.API_HOST and ipaddress.ip_address(request.META['REMOTE_ADDR']).is_private:
+        if request.get_host() == settings.API_HOST or ipaddress.ip_address(request.META['REMOTE_ADDR']).is_private:
             return view_func(request, *args, **kwargs)
         return HttpResponse('Access denied', status=403)
     return authorize
