a42501f86561d7c16df85016193b4bea61d3d980
S. Paquette <spaquett@systemsbiology.org>
2020-02-07 11:02:20 -0800

    -> No default value needed for resulting cohort, it just makes things painful

--- cohorts/migrations/0004_auto_20200206_1357.py
+++ cohorts/migrations/0004_auto_20200206_1357.py
@@ -31,7 +31,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='filter_group',
             name='resulting_cohort',
-            field=models.ForeignKey(default=0, on_delete=django.db.models.deletion.CASCADE, to='cohorts.Cohort'),
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='cohorts.Cohort'),
             preserve_default=False,
         ),
         migrations.AlterField(
