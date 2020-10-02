52b30fb4ada33f403571faf3ff082d9a2c996b26
BillClifford <bcli4d>
2020-10-01 22:22:23 -0700

    Restructures netadata APIs

--- idc_collections/views/views_api.py
+++ idc_collections/views/views_api.py
@@ -41,6 +41,8 @@ def versions_list_api(request):
     idc_data_versions = ImagingDataCommonsVersion.objects.all()
     # versions = DataVersion.objects.get_queryset()
 
+    programs = Program.get_public_programs()
+
     versions_info = {"versions": []}
     for version in idc_data_versions:
         version_data = dict(
@@ -54,8 +56,16 @@ def versions_list_api(request):
         for data_source in version.get_data_sources().filter(source_type='B'):
             data_source_data = dict(
                 name = data_source.name,
+                data_type = dict(DataSetType.DATA_TYPES)[data_source.get_data_types().first()]
+
             )
             version_data['data_sources'].append(data_source_data)
+
+        version_data["programs"]= [{
+            "name": program.name,
+            "short_name": program.short_name,
+            "description": program.description} for program in programs]
+
         versions_info["versions"].append(version_data)
 
     return JsonResponse(versions_info)
@@ -63,7 +73,8 @@ def versions_list_api(request):
 
 @api_auth
 @require_http_methods(["GET"])
-def data_sources_list_api(request):
+def collections_list_api(request):
+    # """ if debug: logger.debug('Called ' + sys._getframe().f_code.co_name) """
 
     try:
         data_version = get_idc_data_version(request.GET.get('idc_data_version', ''))
@@ -75,23 +86,77 @@ def data_sources_list_api(request):
             )
         )
 
-    sources = data_version.get_data_sources().filter(
-        source_type=DataSource.BIGQUERY)
-
-    data_sources_info = {"data_sources": []}
-    for source in sources:
-        source_data = dict(
-                name = source.name,
-                data_type = dict(DataSetType.DATA_TYPES)[source.get_data_types().first()]
+    try:
+        program_name = request.GET.get('program_name', '')
+    except:
+        return JsonResponse(
+            dict(
+                message="Invalid program name {}".format(request.GET.get('program_name', '')),
+                code=400
+            )
         )
-        data_sources_info["data_sources"].append(source_data)
+    collections_info = {"programs": []}
 
-    return JsonResponse(data_sources_info)
+    if program_name:
+        programs = Program.objects.filter(is_public=True, active=True, short_name__iexact=program_name)
+    else:
+        programs = Program.objects.all()
+
+    for program in programs:
+
+        try:
+
+            # program = Program.objects.get(is_public=True, active=True, short_name__iexact=program_name)
+
+            collections = program.collection_set.all()
+
+            collections_list = []
+            for collection in collections:
+                dvs = collection.data_versions.all()
+                data = {
+                    "collection_id": collection.collection_id,
+                    "description": collection.description,
+                    "date_updated": collection.date_updated,
+                    "subject_count": collection.subject_count,
+                    "image_types": collection.image_types,
+                    "cancer_type": collection.cancer_type,
+                    "doi": collection.doi,
+                    "supporting_data": collection.supporting_data,
+                    "species": collection.species,
+                    "location": collection.location,
+                    "active": collection.active,
+                    "collection_type": dict(collection.COLLEX_TYPES)[collection.collection_type],
+                    "owner_id": collection.owner_id,
+                    "idc_data_versions": ["1.0"]}
+                collections_list.append(data)
+
+            collections_info['programs'].append(
+                {
+                    "program_name": program.short_name,
+                    "collections": collections_list
+                 }
+            )
+
+        except ObjectDoesNotExist as e:
+            logger.error("[ERROR] Specified program does not exist")
+            logger.exception(e)
+            collections_info = {
+                "message": "Specified program {} does not exist".format(program_name),
+                "code": 400
+            }
+        except Exception as e:
+            logger.error("[ERROR] While trying to retrieve program details")
+            logger.exception(e)
+            collections_info = {
+                "message": "Error while trying to retrieve program details.",
+                "code": 400
+            }
+    return JsonResponse(collections_info)
 
 
 @api_auth
 @require_http_methods(["GET"])
-def attributes_list_api(request, data_source):
+def attributes_list_api(request):
 
     try:
         data_version = get_idc_data_version(request.GET.get('idc_data_version', ''))
@@ -104,7 +169,7 @@ def attributes_list_api(request, data_source):
         )
 
     try:
-        source = data_version.get_data_sources().get(name=data_source)
+        data_source_name = request.GET.get('data_source')
     except ObjectDoesNotExist:
         return JsonResponse(
             dict(
@@ -113,169 +178,44 @@ def attributes_list_api(request, data_source):
             )
         )
 
+    response = {"data_sources": []}
+
     # attr_data = source.get_source_attrs(with_set_map=False, for_faceting=False)
+    if data_source_name:
+        sources = data_version.get_data_sources().filter(name=data_source_name)
+    else:
+        sources = data_version.get_data_sources().filter(source_type='B')
 
 
-    # attributes = Attribute.objects.all()
-    attributes = source.get_attr(for_faceting=False)
+    for source in sources:
+        # attributes = Attribute.objects.all()
+        attributes = source.get_attr(for_faceting=False)
 
-    attributes_info = []
-    for attribute in attributes:
-        if 'clinical_' in attribute.name:
-            pass
-        attribute_info = {
-            "name": attribute.name,
-            "data_type": dict(Attribute.DATA_TYPES)[attribute.data_type],
-            "active": attribute.active,
-            "units": attribute.units,
-            "idc_data_version": data_version.version_number
+        attributes_info = []
+        for attribute in attributes:
+            if 'clinical_' in attribute.name:
+                pass
+            attribute_info = {
+                "name": attribute.name,
+                "data_type": dict(Attribute.DATA_TYPES)[attribute.data_type],
+                "active": attribute.active,
+                "units": attribute.units,
+                "idc_data_version": data_version.version_number
+            }
+            attributes_info.append(attribute_info)
+            if attribute_info['data_type'] == 'Continuous Numeric':
+                for suffix in ['lt', 'lte', 'btw', 'gte', 'gt']:
+                    attribute_info_copy = dict(attribute_info)
+                    attribute_info_copy['name'] = '{}_{}'.format(attribute.name, suffix)
+                    attributes_info.append(attribute_info_copy)
+        data_source = {
+            "data_source": source.name,
+            'attributes': attributes_info
         }
-        attributes_info.append(attribute_info)
-        if attribute_info['data_type'] == 'Continuous Numeric':
-            for suffix in ['lt', 'lte', 'btw', 'gte', 'gt']:
-                attribute_info_copy = dict(attribute_info)
-                attribute_info_copy['name'] = '{}_{}'.format(attribute.name, suffix)
-                attributes_info.append(attribute_info_copy)
-
-    response = {'attributes': attributes_info}
+        response["data_sources"].append(data_source)
 
 
     return JsonResponse(response)
-
-@api_auth
-@require_http_methods(["GET"])
-def public_program_list_api(request):
-
-    programs = Program.get_public_programs()
-
-    programs_info = {"programs": [{
-                "name": program.name,
-                "short_name": program.short_name,
-                "description": program.description} for program in programs]}
-
-    return JsonResponse(programs_info)
-#    return HttpResponse(programs_info,  content_type='application/json')
-
-
-@api_auth
-@require_http_methods(["GET"])
-def program_detail_api(request, program_name=None ):
-    # """ if debug: logger.debug('Called ' + sys._getframe().f_code.co_name) """
-
-    try:
-        data_version = get_idc_data_version(request.GET.get('idc_data_version', ''))
-    except:
-        return JsonResponse(
-            dict(
-                message="Invalid IDC version {}".format(request.GET.get('idc_data_version', '')),
-                code=400
-            )
-        )
-
-    try:
-
-        program = Program.objects.get(is_public=True, active=True, short_name__iexact=program_name)
-
-        collections = program.collection_set.all()
-
-        collections_list = []
-        for collection in collections:
-            dvs = collection.data_versions.all()
-            data = {
-                "name": collection.name,
-                "collection_id": collection.collection_id,
-                "description": collection.description,
-                "date_updated": collection.date_updated,
-                "subject_count": collection.subject_count,
-                "image_types": collection.image_types,
-                "cancer_type": collection.cancer_type,
-                "doi": collection.doi,
-                "supporting_data": collection.supporting_data,
-                "species": collection.species,
-                "location": collection.location,
-                "active": collection.active,
-                "collection_type": dict(collection.COLLEX_TYPES)[collection.collection_type],
-                "owner_id": collection.owner_id,
-                "idc_data_versions": ["1.0"]}
-            collections_list.append(data)
-
-        collections_info = {"collections": collections_list}
-
-    except ObjectDoesNotExist as e:
-        logger.error("[ERROR] Specified program does not exist")
-        logger.exception(e)
-        collections_info = {
-            "message": "Specified program {} does not exist".format(program_name),
-            "code": 400
-        }
-    except Exception as e:
-        logger.error("[ERROR] While trying to retrieve program details")
-        logger.exception(e)
-        collections_info = {
-            "message": "Error while trying to retrieve program details.",
-            "code": 400
-        }
-
-    return JsonResponse(collections_info)
-
-
-@api_auth
-@require_http_methods(["GET"])
-def collections_list_api(request, idc_data_version=None ):
-    # """ if debug: logger.debug('Called ' + sys._getframe().f_code.co_name) """
-
-    try:
-        data_version = get_idc_data_version(request.GET.get('idc_data_version', ''))
-    except:
-        return JsonResponse(
-            dict(
-                message="Invalid IDC version {}".format(request.GET.get('idc_data_version', '')),
-                code=400
-            )
-        )
-
-    try:
-        # collections = program.collection_set.all()
-        collections = Collection.objects.all()
-
-        collections_list = []
-        for collection in collections:
-            dvs = collection.data_versions.all()
-            data = {
-                "collection_id": collection.collection_id,
-                "description": collection.description,
-                "date_updated": collection.date_updated,
-                "subject_count": collection.subject_count,
-                "image_types": collection.image_types,
-                "cancer_type": collection.cancer_type,
-                "doi": collection.doi,
-                "supporting_data": collection.supporting_data,
-                "species": collection.species,
-                "location": collection.location,
-                "active": collection.active,
-                "collection_type": dict(collection.COLLEX_TYPES)[collection.collection_type],
-                "owner_id": collection.owner_id,
-                "idc_data_versions": ["1.0"]}
-            collections_list.append(data)
-
-        collections_info = {"collections": collections_list}
-
-    except ObjectDoesNotExist as e:
-        logger.error("[ERROR] Specified program does not exist")
-        logger.exception(e)
-        collections_info = {
-            "message": "Specified program does not exist",
-            "code": 400
-        }
-    except Exception as e:
-        logger.error("[ERROR] While trying to retrieve program details")
-        logger.exception(e)
-        collections_info = {
-            "message": "Error while trying to retrieve program details.",
-            "code": 400
-        }
-
-    return JsonResponse(collections_info)
 
 
 
