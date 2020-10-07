06a7f3804c0bc16eb89191e940cc9d06e23d4210
S. Paquette <spaquett@systemsbiology.org>
2020-10-07 02:23:19 -0700

    -> Attribute display values and names for filters in manifests

--- cohorts/models.py
+++ cohorts/models.py
@@ -154,17 +154,22 @@ class Cohort(models.Model):
         filter_groups = self.filter_group_set.all()
         filter_sets = []
 
-        ar_dvals = Attribute_Display_Values.objects.select_related('attribute').filter(attribute=Attribute.objects.get(name="AnatomicRegionSequence")).to_dict()
+        attr_dvals = Attribute_Display_Values.objects.select_related('attribute').filter(
+            attribute__id__in=Attribute.objects.filter(id__in=self.get_attr_list())
+        ).to_dict()
 
         for fg in filter_groups:
             filters = fg.filter_set.all().get_filter_set_array()
-            group_filters = {x['name']: [ar_dvals.get(x['id'],{}).get(y,y) for y in x['values']] for x in filters}
+            group_filters = {x['display_name']: [attr_dvals.get(x['id'],{}).get(y,y) for y in x['values']] for x in filters}
 
             filter_sets.append(BigQuerySupport.build_bq_where_clause(
                 group_filters, field_prefix=prefix
             ))
 
         return " AND ".join(filter_sets).replace("AnatomicRegionSequence","AnatomicRegion")
+
+    def get_attr_list(self):
+        return self.filter_set.select_related('attribute').all().values_list('attribute__id',flat=True)
 
     # Produce a BigQuery filter WHERE clause for this cohort's filters that can be used in the BQ console
     def get_bq_filter_string(self, prefix=None):
