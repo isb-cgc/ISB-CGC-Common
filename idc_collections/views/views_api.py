a66b636c11f0dde8c7f5f2d92cee3f3c72a840f2
BillClifford <bcli4d>
2021-09-16 17:03:29 -0700

    Generally removeIDC view dependencies.

--- idc_collections/views/views_api.py
+++ idc_collections/views/views_api.py
@@ -43,18 +43,19 @@ def versions_list_api(request):
                 idc_data_version = version.version_number,
                 # version_uid = version.version_uid,
                 date_active = version.date_active,
-                active = version.active,
-                data_sources = []
+                active = version.active
+                # active = version.active,
+                # data_sources = []
         )
-        for data_source in version.get_data_sources().filter(source_type='B').distinct():
-            if data_source.name == "idc-dev.metadata.dicom_pivot_wave0":
-                continue
-            data_source_data = dict(
-                name = data_source.name,
-                data_type = dict(DataSetType.DATA_TYPES)[data_source.get_data_types().first()]
-
-            )
-            version_data['data_sources'].append(data_source_data)
+        # for data_source in version.get_data_sources().filter(source_type='B').distinct():
+        #     if data_source.name == "idc-dev.metadata.dicom_pivot_wave0":
+        #         continue
+        #     data_source_data = dict(
+        #         name = data_source.name,
+        #         data_type = dict(DataSetType.DATA_TYPES)[data_source.get_data_types().first()]
+        #
+        #     )
+        #     version_data['data_sources'].append(data_source_data)
 
         versions_info["versions"].append(version_data)
 
@@ -108,16 +109,18 @@ def analysis_results_list_api(request):
     # The webap DB does not currently map collections to IDC versions
     # Until that mapping is included, we do that here
 
-    try:
-        data_version = get_idc_data_version(request.GET.get('idc_data_version', ''))
-    except:
-        return JsonResponse(
-            dict(
-                message="Invalid IDC version {}".format(request.GET.get('idc_data_version', '')),
-                code=400
-            )
-        )
+    data_version = get_idc_data_version('')
 
+    # try:
+    #     data_version = get_idc_data_version(request.GET.get('idc_data_version', ''))
+    # except:
+    #     return JsonResponse(
+    #         dict(
+    #             message="Invalid IDC version {}".format(request.GET.get('idc_data_version', '')),
+    #             code=400
+    #         )
+    #     )
+    #
     collections_info = {"analysisResults": []}
     programs = Program.objects.all()
 
@@ -158,15 +161,16 @@ def analysis_results_list_api(request):
 @require_http_methods(["GET"])
 def attributes_list_api(request):
 
-    try:
-        data_version = get_idc_data_version(request.GET.get('idc_data_version', ''))
-    except:
-        return JsonResponse(
-            dict(
-                message="Invalid IDC version {}".format(request.GET.get('idc_data_version')),
-                code=400
-            )
-        )
+    data_version = get_idc_data_version('')
+    # try:
+    #     data_version = get_idc_data_version(request.GET.get('idc_data_version', ''))
+    # except:
+    #     return JsonResponse(
+    #         dict(
+    #             message="Invalid IDC version {}".format(request.GET.get('idc_data_version')),
+    #             code=400
+    #         )
+    #     )
 
     try:
         data_source_name = request.GET.get('data_source')
@@ -208,7 +212,7 @@ def attributes_list_api(request):
 
     for source in sources:
         # attributes = Attribute.objects.all()
-        attributes = source.get_attr(for_faceting=False)
+        attributes = source.get_attr(for_faceting=False).filter(default_ui_display=True) 
 
         attributes_info = []
         for attribute in attributes:
@@ -222,7 +226,7 @@ def attributes_list_api(request):
             }
             attributes_info.append(attribute_info)
             if attribute_info['data_type'] == 'Continuous Numeric':
-                for suffix in ['lt', 'lte', 'btw', 'gte', 'gt']:
+                for suffix in ['lt', 'lte', 'btw', 'ebtw', 'ebtwe', 'btwe', 'gte', 'gt']:
                     attribute_info_copy = dict(attribute_info)
                     attribute_info_copy['name'] = '{}_{}'.format(attribute.name, suffix)
                     attributes_info.append(attribute_info_copy)
