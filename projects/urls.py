9d1204dc07aa428e3a5cca8a3eccd3184650a175
S. Paquette <spaquett@systemsbiology.org>
2025-02-28 10:08:32 -0800

    -> Django 4.2

--- projects/urls.py
+++ projects/urls.py
@@ -1,11 +1,11 @@
 from __future__ import absolute_import
-from django.conf.urls import url
+from django.urls import re_path
 
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.program_list, name='programs'),
-    url(r'^public/$', views.program_list, name='public_programs'),
-    url(r'^(?P<program_id>\d+)/$', views.program_detail, name='program_detail'),
-    url(r'^system_data_dict/$', views.system_data_dict, name='system_data_dict'),
+    re_path(r'^$', views.program_list, name='programs'),
+    re_path(r'^public/$', views.program_list, name='public_programs'),
+    re_path(r'^(?P<program_id>\d+)/$', views.program_detail, name='program_detail'),
+    re_path(r'^system_data_dict/$', views.system_data_dict, name='system_data_dict'),
 ]
--- sharing/urls.py
+++ sharing/urls.py
@@ -1,9 +1,9 @@
 from __future__ import absolute_import
-from django.conf.urls import url
+from django.urls import re_path
 
 from . import views
 
 urlpatterns = [
-    url(r'^(?P<sharing_id>\d+)/$', views.sharing_add, name='sharing_add'),
-    url(r'^(?P<sharing_id>\d+)/remove$', views.sharing_remove, name='sharing_remove'),
+    re_path(r'^(?P<sharing_id>\d+)/$', views.sharing_add, name='sharing_add'),
+    re_path(r'^(?P<sharing_id>\d+)/remove$', views.sharing_remove, name='sharing_remove'),
 ]
